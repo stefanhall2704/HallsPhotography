d2ff19ef23f821df61d6574e7aa242fc65300c4f
Stefan Hall <stefan.hall@cfainstitute.org>
2023-06-06 16:46:05 -0400

    database migration for photo table

--- photography/hallsPhotography/models.py
+++ photography/hallsPhotography/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Photo(models.Model):
+    id = models.AutoField(primary_key=True)
+    watermarked_photo = models.UUIDField()
+    non_watermarked_photo = models.UUIDField()
